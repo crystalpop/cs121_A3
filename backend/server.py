c67d328128f099509fea520437ba5d830ecc35cc
Linden Tran <linht11@uci.edu>
2025-03-09 20:32:15 -0700

    new server.py

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import time
-from backend.A3part2 import BooleanSearch
 import os
 from A3part2 import SearchEngine  # Use SearchEngine instead of BooleanSearch
 
@@ -15,12 +13,8 @@ INDEX_FILE = os.path.join(BASE_DIR, "index_files2/final_inverted_index.json")
 METADATA_FILE = os.path.join(BASE_DIR, "docID_dict.json")
 TERM_OFFSETS_FILE = os.path.join(BASE_DIR, "term_offsets.json")
 
-# Ensure files exist before loading the search engine
-if not os.path.exists(INDEX_FILE) or not os.path.exists(METADATA_FILE) or not os.path.exists(TERM_OFFSETS_FILE):
-    raise FileNotFoundError("One or more required index files are missing in the backend deployment.")
-
-# Initialize the Boolean Search Engine
-search_engine = BooleanSearch(INDEX_FILE, METADATA_FILE, TERM_OFFSETS_FILE)
+# Initialize the Search Engine
+search_engine = SearchEngine(INDEX_FILE, METADATA_FILE, TERM_OFFSETS_FILE)
 
 @app.route("/search", methods=["GET"])
 def search():
